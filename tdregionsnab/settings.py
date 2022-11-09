4afb7d77091ebd129e6880a4ccfa5998630096fb
Михаил Игнатьев <mishkamba1@gmail.com>
2022-11-09 16:29:00 +0900

    pagination

--- tdregionsnab/settings.py
+++ tdregionsnab/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 INSTALLED_APPS += ('django_summernote', )
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
 }
 
